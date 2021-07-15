0ae22b0df11696dcb61a69a8d613fa77c676205f
Anri <anrihunanyan@gmail.com>
2021-07-15 15:56:04 +0400

    feat(Inputs.jsx): created Input component

---
+++ src/components/Inputs/Inputs.jsx
@@ -0,0 +1,6 @@
+export default function Input({placeholder, value, onChange}) {
+      return  (<input type="number" 
+      placeholder ={placeholder}
+      value={value} 
+      onChange={onChange}></input>)
+}
