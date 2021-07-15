a4c60dee4c65348534df02c10e2fcf39288f17e9
Anri <anrihunanyan@gmail.com>
2021-07-15 16:38:25 +0400

    feat(input) added styles for input

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -1,5 +1,8 @@
+import style from "./Input.module.css";
+
+
 export default function Input({placeholder, value, onChange}) {
-      return  (<input type="number" 
+      return  (<input className={style.Input} type="number" 
       placeholder ={placeholder}
       value={value} 
       onChange={onChange}></input>)
